efb261570ec51f0a6342c32f4c4eb4ff79a7b993
Ovler <ovlertheyoung@gmail.com>
2024-11-19 02:28:44 -0500

    test: refactor tests for collection items and item metadata retrieval with parameterization
    Co-authored-by: yzqzss <yzqzss@users.noreply.github.com>
    
    @yzqzss really helps a lot!
    He had helped about parametrize, and I have done the others!

--- tests/test_libs.py
+++ tests/test_libs.py
@@ -6,22 +6,38 @@ from ia_collection_analyzer.libs import (
 )
 
 
-def test_get_collection_items():
-    collection_id = "us_senate"
+get_collection_items_tests = [
+    ("speedydeletionwiki", 8, "wikipedia-delete-v3-2012-08"),
+    ("us_senate",  3232, "armedA011614_1"),
+]
+
+get_item_metadata_tests = [
+    ("wikipedia-delete-v2-2012-12", "speedydeletionwiki"),
+    ("wikipedia-delete-v3-2012-08", "speedydeletionwiki"),
+    ("wikipedia-delete-v3-2012-07", "speedydeletionwiki"),
+]
+
+
+@pytest.mark.parametrize("collection_id, expected_min_length, one_expected_id", get_collection_items_tests)
+def test_get_collection_items(collection_id, expected_min_length, one_expected_id):
     items = get_collection_items(collection_id)
-    assert len(items) >= 3232
+    assert isinstance(items, list)
+    assert len(items) >= expected_min_length
+    assert one_expected_id in items
 
 
-def test_get_item_metadata():
-    collection_id = "us_senate"
-    item_id = "armedA011614_1"
+@pytest.mark.parametrize("item_id, collection_id", get_item_metadata_tests)
+def test_get_item_metadata(item_id, collection_id):
     metadata = get_item_metadata(item_id)
+    assert isinstance(metadata, dict)
     assert collection_id in metadata["collection"]
 
 
 def test_get_collection_items_metadata():
     collection_id = "speedydeletionwiki"
     items_metadata = get_collection_items_metadata(collection_id)
+    assert isinstance(items_metadata, list)
     assert len(items_metadata) >= 8
     for item_metadata in items_metadata:
+        assert isinstance(item_metadata, dict)
         assert collection_id in item_metadata["collection"]
