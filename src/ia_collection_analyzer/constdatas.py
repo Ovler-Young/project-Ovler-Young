71022c858ebcac7043bdb700b446e8fc5b5d1587
Ovler <ovlertheyoung@gmail.com>
2024-11-29 22:41:26 -0500

    chore: update COLLECTION_TTL to 30 days and modify REQUIRED_METADATA comments

--- src/ia_collection_analyzer/constdatas.py
+++ src/ia_collection_analyzer/constdatas.py
@@ -5,12 +5,12 @@ CACHE_DIR = Path("cache")
 ITEM_CACHE_DIR =  CACHE_DIR / "item"
 
 # Cache configuration 
-COLLECTION_TTL = 7 * 24 * 3600  # 7 * 24 hours in seconds
+COLLECTION_TTL = 30 * 24 * 3600  # 30 * 24 hours in seconds
 
 # Required metadata fields
 REQUIRED_METADATA = [
     "addeddate",
-    "publicdate", 
+    # "publicdate",  # don't have too much meaning for our analyze
     "identifier",
     "mediatype",
     # "uploader", # IA removed this field automatically.
